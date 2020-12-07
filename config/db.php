c60c512d804d0fc7456a09269f4be1fc7fd9a88f
Kutepov <vadim.kutepov@gmail.com>
2020-12-07 21:06:56 +0500

    Init

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db;dbname=food',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
---
+++ migrations/m201207_072552_create_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m201207_072552_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string(255),
+            'access_token' => $this->string(255),
+            'auth_key' => $this->string(255)
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
